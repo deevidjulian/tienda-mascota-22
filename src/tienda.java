b1b79c38a685745b8174959f8314812fcd5b0612
DEevid20 <defernandodelatorre06@gmail.com>
2026-02-23 21:23:03 -0500

    Primer commit

---
+++ src/tienda.java
@@ -0,0 +1,29 @@
+public class tienda {
+
+        private String nombre;
+        private String especie;
+        private int edad;
+        private double peso;
+        private boolean vacunado;
+
+        //constructor
+public tienda(String nombre,String especie,int edad,double peso)    {
+
+    this.nombre= nombre;
+    this.especie= especie;
+    this.edad= edad;
+    this.peso= peso;
+    this.vacunado= false;
+
+}
+public  void vacunar(){
+    if(vacuando){
+        vacuando = true ;
+
+    }
+
+}
+
+
+
+}
